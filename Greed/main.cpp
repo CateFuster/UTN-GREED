b5716efa57ae4800d9debb0a8a7de54888f36286
sofiapastorconde <pastor.sofi@gmail.com>
2024-10-26 16:57:05 -0300

    Menu principal

--- Greed/main.cpp
+++ Greed/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+#include "menu_principal.h"
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+int const TAM=5,BLOQ=2;
+srand(time(0));
+int dados[TAM]{},dadosBloq[BLOQ]{};
+char jugador1, jugador2;
+
+menuPrincipal();
+
+
+
     return 0;
 }
---
+++ Greed/menu_principal.h
@@ -0,0 +1,23 @@
+#ifndef MENU_PRINCIPAL_H_INCLUDED
+#define MENU_PRINCIPAL_H_INCLUDED
+
+ /// MENU PRINCIPAL
+void menuPrincipal(){
+int opcion = 0;
+
+system("cls");
+cout<<"----MENU PRINCIPAL----"<<endl;
+cout<<"1 - MODO UN JUGADOR"<<endl;
+cout<<"2 - MODO DOS JUGADORES"<<endl;
+cout<<"3 - ESTADISTICAS"<<endl;
+cout<<"4 - CREDITOS"<<endl;
+cout<<"----------------------"<<endl;
+cout<<"SALIR"<<endl;
+cout<<"Opcion: ";
+cin>>opcion;
+
+ }
+
+
+
+#endif // MENU_PRINCIPAL_H_INCLUDED
