44cb5f6139e7ee133cd28fd577c9dc60966dc117
Ezequiel Boyer <138337278+Echulp@users.noreply.github.com>
2024-11-12 07:50:36 -0300

    Update menu_principal.h
    Se deja solamente la funcion y se coloca todo lo demas en menu_principal.cpp

--- Greed/menu_principal.h
+++ Greed/menu_principal.h
@@ -1,26 +1,4 @@
-#ifndef MENU_PRINCIPAL_H_INCLUDED
-#define MENU_PRINCIPAL_H_INCLUDED
+#pragma once
 
  /// MENU PRINCIPAL
-int menuPrincipal(){
-int opcion = 0;
-
-system("cls");
-cout<<"----MENU PRINCIPAL----"<<endl;
-cout<<"1 - MODO UN JUGADOR"<<endl;
-cout<<"2 - MODO DOS JUGADORES"<<endl;
-cout<<"3 - ESTADISTICAS"<<endl;
-cout<<"4 - CREDITOS"<<endl;
-cout<<"----------------------"<<endl;
-cout<<"0 - SALIR"<<endl<<endl;
-cout<<"Opcion: ";
-
-cin>>opcion;
-
-return opcion;
- }
-
-
-
-
-#endif // MENU_PRINCIPAL_H_INCLUDED
+int menuPrincipal();
