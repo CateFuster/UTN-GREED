09613283df8c9c840dd98af8fab24a5e2d192fd3
Ezequiel Boyer <138337278+Echulp@users.noreply.github.com>
2024-11-12 07:52:10 -0300

    Add files via upload
    Se agregan los respectivos archivos .cpp

---
+++ Greed/menu_principal.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+ /// MENU PRINCIPAL
+int menuPrincipal(){
+    int opcion = 0;
+
+    system("cls");
+    cout<<"----MENU PRINCIPAL----"<<endl;
+    cout<<"1 - MODO UN JUGADOR"<<endl;
+    cout<<"2 - MODO DOS JUGADORES"<<endl;
+    cout<<"3 - ESTADISTICAS"<<endl;
+    cout<<"4 - CREDITOS"<<endl;
+    cout<<"----------------------"<<endl;
+    cout<<"0 - SALIR"<<endl<<endl;
+    cout<<"Opcion: ";
+
+    cin>>opcion;
+
+    return opcion;
+ }
---
+++ Greed/partida.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include "partida.h"
+
+using namespace std;
+
+void mostrarNombre(string nombre, int numero){
+    cout << "Jugador #" << numero << " - " << nombre;
+    cout << endl << "-------------------------" << endl;
+}
+
+int tirarDado(){
+    return rand() % 6 + 1;
+}
+
+void tirarBloq(int vDadosBloq[], int cantidad){
+    for(int i=0; i<cantidad; i++ ){
+       vDadosBloq[i] = tirarDado();
+    }
+}
+
+void bloqMostrar(int vDadosBloq[], int cantidad){
+    cout << "DADOS BLOQUEADORES: ";
+    for(int i=0;i<cantidad;i++){
+        cout<<vDadosBloq[i]<<" ";
+    }
+    cout << endl << "--------" << endl;
+}
+
+void tirarDados(int vDados[], int cantidad, int vDadosBloq[]){
+    int dado;
+    for (int i=0; i<cantidad; i++){
+        dado = tirarDado();
+        if (validarDado(dado, vDadosBloq,2)){
+
+        }
+    }
+}
+
+bool validarDado(int dado, int vDadosBloq[], int cantidadBloq){
+    for (int i=0; i<cantidadBloq; i++){
+        if (vDadosBloq[i] == dado){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool validarDados(int vDados[], int vDadosBloq[]){
+    bool control[5];
+    for (int i=0; i<6; i++){
+        control[i] = validarDado(vDados[i], vDadosBloq, 2);
+    }
+    return control;
+}
+
+/// Jugada
+
+void jugar(string nombre, int nroJugador){
+
+    bool continuarJugando = true;
+    int dados[5], bloqueadores[2], nroLanzamiento=1, puntosJugada, puntosTotal;
+
+    mostrarNombre(nombre, nroJugador);
+    tirarBloq(bloqueadores, 2);
+    bloqMostrar(bloqueadores, 2);
+
+    while (continuarJugando == true) {
+        cout << "#" << nroLanzamiento << "   ";
+        tirarDados(dados,5,bloqueadores);
+        /// mostrar dados sin cambiar los dados que salgan bloqueados
+
+        nroLanzamiento++;
+
+        cout << endl << "Presione cualquier tecla para continuar ...";
+        system("pause>nul");
+        cout << endl;
+
+    }
+}
+
+
+
+    ///Puntos
+
+int sumaDeDados(int *v){
+    int acumulador=0;
+    for(int i=0;i<6;i++){
+                acumulador+=v[i];
+    }
+     return acumulador;
+}
+void verPuntaje(int puntaje){
+    cout<<endl;
+    cout<<"PUNTAJE OBTENIDO: "<<puntaje;
+    cout<<endl;
+}
