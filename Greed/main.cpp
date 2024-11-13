8d31da6a02d3dea20067da0ebe4761733e1e05cc
sofiapastorconde <pastor.sofi@gmail.com>
2024-11-13 16:26:47 -0300

    actualizacion menu 2

--- Greed/main.cpp
+++ Greed/main.cpp
@@ -8,51 +8,14 @@ using namespace std;
 
 int main(){
 
-int const TAM=5,BLOQ=2;
-srand(time(0));
 int opcion;
-int dados[TAM]{},dadosBloq[BLOQ]{};
 char jugador1, jugador2;
+
 
 do{
    opcion = menuPrincipal();
-
-switch(opcion){
-     case 1:
-                /// MODO UN JUGADOR
-                cout<<endl<<"INGRESE NOMBRE JUGADOR: ";
-                cin >> jugador1;
-               // tirarBloq()
-
-
-
-                system("pause");
-                break;
-    case 2:
-                /// MODO DOS JUGADORES
-                cout<<endl<<"INGRESE NOMBRE JUGADOR 1: ";
-                cin >> jugador1;
-                cout<<endl<<"INGRESE NOMBRE JUGADOR 2: ";
-                cin >> jugador2;
-               break;
-    case 3:
-                ///ESTADISTICAS
-                break;
-    case 4:
-               ///CREDITOS
-               cout << endl<<"Juego realizado por: "<<endl<<endl;
-               cout << "Caterina Fuster"<<endl;
-               cout << "Ezequiel Boyer"<<endl;
-               cout << "Sofia Pastor Conde"<<endl;
-                      break;
-    case 0:
-        ///CIERRE JUEGO
-        cout << "Gracias por participar"<<endl;
-
-    default:
-        cout << "Opción inválida"<<endl;
-
-}}
+   ejecutarOpcion (opcion, jugador1, jugador2);
+   }
     while (opcion != 0);
 
  return 0;
--- Greed/menu_principal.h
+++ Greed/menu_principal.h
@@ -20,6 +20,45 @@ cin>>opcion;
 return opcion;
  }
 
+ void ejecutarOpcion(int opcion, char jugador1, char jugador2){
+ switch(opcion){
+     case 1:
+                /// MODO UN JUGADOR
+                cout<<endl<<"INGRESE NOMBRE JUGADOR: ";
+                cin >> jugador1;
+               // tirarBloq()
+
+                system("pause");
+                break;
+    case 2:
+                /// MODO DOS JUGADORES
+                cout<<endl<<"INGRESE NOMBRE JUGADOR 1: ";
+                cin >> jugador1;
+                cout<<endl<<"INGRESE NOMBRE JUGADOR 2: ";
+                cin >> jugador2;
+               break;
+    case 3:
+                ///ESTADISTICAS
+                break;
+    case 4:
+               ///CREDITOS
+               cout << endl<<"Juego realizado por: "<<endl<<endl;
+               cout << "Caterina Fuster"<<endl;
+               cout << "Ezequiel Boyer"<<endl;
+               cout << "Sofia Pastor Conde"<<endl;
+                      break;
+    case 0:
+        ///CIERRE JUEGO
+        cout << "Gracias por jugar"<<endl;
+        break;
+
+    default:
+        cout << "Opción inválida"<<endl;
+        break;
+
+}
+ }
+
 
 
 
