1946ad02b86d7953820d0765060d0f3cac9be78b
Ezequiel Boyer <138337278+Echulp@users.noreply.github.com>
2024-11-15 07:09:21 -0300

    Update partida.h
    Se incluye <string> y se agrega el uso de namespace std, se agrega la declacracion de la funcion carrgarNombre y la de mostrar dados

--- Greed/partida.h
+++ Greed/partida.h
@@ -1,6 +1,11 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 
 void solicitarNombreJugador();
+void cargarNombre(string jugador[], int tam, int jug);
 void mostrarNombre(string nombre, int numero);
 int tirarDado();
 void tirarBloq(int vDadosBloq[], int cantidad);
@@ -9,6 +14,7 @@ void tirarDados(int vDados[], int cantidad, int vDadosBloq[]);
 bool validarDado(int dado, int vDadosBloq[], int cantidadBloq);
 bool validarDados(int vDados, int vDadosBloq);
 void jugar(string nombre, int nroJugador);
+void mostrarDados(int vDados[], int cantidad);
 int sumaDeDados(int *v);
 void verPuntaje(int puntaje);
 
